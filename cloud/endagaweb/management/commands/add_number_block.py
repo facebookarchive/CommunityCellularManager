bbde5592a0b0ec73b9f0fe903604508cdf81f932
Steve Muir <muir@fb.com>
2017-02-28 12:57:49 -0800

    Add option to set number kind
    Summary:
    When adding numbers to the db it's necessary to set the kind field so
    that Freeswitch handles call routing correctly. The default is to add numbers as
    'local' rather than 'number.telecom.permanent' so that we don't accidentally send
    a load of junk traffic to the telecom partner's SIP gateway.
    
    Reviewed By: kkroo
    
    Differential Revision: D4627762
    
    fbshipit-source-id: 601d5224a4c6493f9117cb5866292e4f5c795242

--- cloud/endagaweb/management/commands/add_number_block.py
+++ cloud/endagaweb/management/commands/add_number_block.py
@@ -4,13 +4,17 @@ This is always a contiguous range. Skips numbers that are already present.
 
 Examples:
     python manage.py add_number_block <start_num> <block_size> <country_id>
-    python manage.py add_number_block 639360130000 1000 PH
+    python manage.py add_number_block 639360130000 1000 PH --kind <kind>
+
+Use the '--kind' optional argument to specify the kind of number, e.g.,
+number.telecom.permanent for a number that the cloud routes via a known
+SIP gateway.
 
 Copyright (c) 2016-present, Facebook, Inc.
 All rights reserved.
 
 This source code is licensed under the BSD-style license found in the
-LICENSE file in the root directory of this source tree. An additional grant 
+LICENSE file in the root directory of this source tree. An additional grant
 of patent rights can be found in the PATENTS file in the same directory.
 """
 
@@ -37,22 +41,28 @@ class Command(BaseCommand):
         parser.add_argument('start_num', type=int)
         parser.add_argument('block_size', type=int)
         parser.add_argument('country_id', type=_is_two_letter_string)
+        parser.add_argument('--kind',
+                            type=str,
+                            default='local',
+                            help='Kind of number, e.g., number.nexmo.monthly')
 
     def handle(self, *args, **options):
         start_num = options['start_num']
         block_size = options['block_size']
         country_id = options['country_id']
+        kind = options['kind']
 
         for i in range(0, block_size):
             num = str(start_num + i)
             if Number.objects.filter(number=num).exists():
                 print "Skipping %s, already in DB." % num
                 continue
-            n = Number(subscriber = None,
-                       network = None,
-                       number = str(num),
-                       state = "available",
-                       country_id = country_id)
+            n = Number(subscriber=None,
+                       network=None,
+                       number=str(num),
+                       state="available",
+                       country_id=country_id,
+                       kind=kind)
 
             n.save()
             print "Added %s to DB." % (unicode(n),)
